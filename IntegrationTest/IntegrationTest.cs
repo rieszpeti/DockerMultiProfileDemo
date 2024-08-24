fdf9b9cc7ca2e0f3a9deb0d04c09e6abc843abad
SillySharp <rieszpeti@gmail.com>
2024-08-24 15:36:16 +0200

    add env file to inject secrets and finish integration test

--- IntegrationTest/IntegrationTest.cs
+++ IntegrationTest/IntegrationTest.cs
@@ -1,25 +1,32 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace IntegrationTest;
 
 public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
+    private readonly string _domainName;
 
     public IntegrationTests(WebApplicationFactory<Program> factory)
     {
         _factory = factory;
+
+        _domainName = "http://myapp.local"; // TODO hardcoded
     }
 
     [Fact]
     public async Task GetWeatherForecastReturnsSuccessAndCorrectContentType()
     {
-        //Arrange
-        var client = _factory.CreateClient();
-        //Act
+        // Arrange
+        var client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            BaseAddress = new Uri(_domainName)
+        });
+
+        // Act
         var response = await client.GetAsync("WeatherForecast");
-        //Assert
+
+        // Assert
         response.EnsureSuccessStatusCode();
         Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
     }
